e70bca10577545c64cbd2ce09287edb3edf0a6f4
AhmedBoshra <ahmedboshra96@gmail,com>
2023-05-15 11:51:44 +0200

    uploading initial files

---
+++ app.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000);
